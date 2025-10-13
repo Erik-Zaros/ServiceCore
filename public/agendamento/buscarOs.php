8cdec6b0b08b601cf9d8507064fffbe1c906604e
Erik Delanda Zaros <erikzaros942@gmail.com>
2025-10-13 20:43:36 -0300

    Ajuste agendamento

--- public/agendamento/buscarOs.php
+++ public/agendamento/buscarOs.php
@@ -6,7 +6,14 @@ use App\Auth\Autenticador;
 Autenticador::iniciar();
 $con = Db::getConnection();
 
-$sql = "SELECT os, nome_consumidor FROM tbl_os WHERE finalizada IS FALSE AND cancelada IS FALSE ORDER BY os DESC";
+$sql = "SELECT os,
+			   nome_consumidor
+		FROM tbl_os
+		WHERE finalizada IS FALSE
+		AND cancelada IS FALSE
+		AND NOT EXISTS (SELECT 1 FROM tbl_agendamento WHERE tbl_agendamento.os = tbl_os.os AND tbl_agendamento.status NOT IN('CANCELADO'))
+		ORDER BY os ASC;
+	";
 $res = pg_query($con, $sql);
 
 $out = [];
