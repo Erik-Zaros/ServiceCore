48df9d5365947e89efd3fbde4a744172ce1c83ec
Erik Delanda Zaros <erikzaros942@gmail.com>
2025-11-03 19:44:44 -0300

    removendo relatorio do menu

--- view/menu.php
+++ view/menu.php
@@ -11,36 +11,31 @@ $pageTitle = 'MENU';
 ob_start();
 ?>
 
-    <div class="text-center mb-3">
-        <a href="../public/menu/relatorio.php" class="btn btn-success btn-sm">Download Excel</a>
-    </div>
-
-    <div class="row">
-        <div class="col-md-4">
-            <div class="card p-3 shadow-sm border-0 card-sharp">
-                <h5 class="text-center">Status das OS</h5>
-                <div id="grafico-pizza-os-status" style="height: 300px;"></div>
-            </div>
-        </div>
-        <div class="col-md-4">
-            <div class="card p-3 shadow-sm border-0 card-sharp">
-                <h5 class="text-center">Status dos Produtos</h5>
-                <div id="grafico-pizza-status-produto" style="height: 300px;"></div>
-            </div>
-        </div>
-        <div class="col-md-4">
-            <div class="card p-3 shadow-sm border-0 card-sharp">
-                <h5 class="text-center">Status das Peças</h5>
-                <div id="grafico-pizza-status-peca" style="height: 300px;"></div>
-            </div>
-        </div>
-        <div class="col-md-12 mt-4">
-            <div class="card p-3 shadow-sm border-0 card-sharp">
-                <h5 class="text-center">Todos os Registros</h5>
-                <div id="grafico-colunas" style="height: 300px;"></div>
-            </div>
-        </div>
-    </div>
+<div class="row mt-4">
+	<div class="col-md-4">
+		<div class="card p-3 shadow-sm border-0 card-sharp">
+			<h5 class="text-center">Status das OS</h5>
+			<div id="grafico-pizza-os-status" style="height: 300px;"></div>
+		</div>
+	</div>
+	<div class="col-md-4">
+		<div class="card p-3 shadow-sm border-0 card-sharp">
+			<h5 class="text-center">Status dos Produtos</h5>
+			<div id="grafico-pizza-status-produto" style="height: 300px;"></div>
+		</div>
+	</div>
+	<div class="col-md-4">
+		<div class="card p-3 shadow-sm border-0 card-sharp">
+			<h5 class="text-center">Status das Peças</h5>
+			<div id="grafico-pizza-status-peca" style="height: 300px;"></div>
+		</div>
+	</div>
+	<div class="col-md-12 mt-4">
+		<div class="card p-3 shadow-sm border-0 card-sharp">
+			<h5 class="text-center">Todos os Registros</h5>
+			<div id="grafico-colunas" style="height: 300px;"></div>
+		</div>
+	</div>
 </div>
 
 <?php
